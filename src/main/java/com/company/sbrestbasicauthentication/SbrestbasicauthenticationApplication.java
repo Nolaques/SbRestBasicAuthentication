823c150d8036882d87b8ebbd06f025b4e95b9a6a
Кирилл Беляк <nolak19@gmail.com>
2018-07-10 10:47:38 +0300

    Initial commit

---
+++ src/main/java/com/company/sbrestbasicauthentication/SbrestbasicauthenticationApplication.java
@@ -0,0 +1,12 @@
+package com.company.sbrestbasicauthentication;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SbrestbasicauthenticationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SbrestbasicauthenticationApplication.class, args);
+	}
+}
